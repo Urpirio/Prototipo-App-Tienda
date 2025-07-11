b0d512d7b82d7abaa5ba08dbeb771be3ec9cf25e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-11 14:44:53 -0400

    Se agrego la base para la ventana de la Tienda

---
+++ app/screens/ScreenStore.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function ScreenStore() {
+  return (
+    <SafeAreaProvider>
+        
+    </SafeAreaProvider>
+  )
+}
